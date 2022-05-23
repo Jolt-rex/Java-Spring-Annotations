4da2392e150cc73d800940616dfcecfaa97d272b
Lee <leepotter010@gmail.com>
2022-05-23 10:11:59 +1000

    Autowiring and Qualifiers added

---
+++ src/com/Jolt/springdemo/FortuneService.java
@@ -0,0 +1,6 @@
+package com.Jolt.springdemo;
+
+public interface FortuneService {
+	
+	public String getFortune();
+}
--- src/com/Jolt/springdemo/TennisCoach.java
+++ src/com/Jolt/springdemo/TennisCoach.java
@@ -1,14 +1,46 @@
 package com.Jolt.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
-// default bean id will be tennisCoach
+// default bean id will be "tennisCoach"
 @Component
 public class TennisCoach implements Coach {
+	
+	// dependency injection directly to field
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+	
+	public TennisCoach() {
+		System.out.println("TennisCoach: inside default constructor");
+	}
+	
+	// automatic constructor injection
+	/*
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	*/
+	
+	// automatic setter injection
+	/*
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 }
